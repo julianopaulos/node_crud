e7cd3cf215a8697b255a58bc49e0e3dda5d954c6
julianopaulos <julianopaulo.santos@hotmail.com>
2022-01-13 09:07:40 -0300

    adicionado env na createdb

--- src/Model/Conn/createDb.js
+++ src/Model/Conn/createDb.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2/promise');
 
-const host = 'localhost';
-const user = 'root';
-const password = '';
-const port = 3306;
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const port = process.env.DB_PORT;
 
 const createConn = {
     async create() {
         try{
             const connection = await mysql.createConnection({ host, port, user, password });
-            await connection.query(`CREATE DATABASE IF NOT EXISTS crud;`);
+            await connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME};`);
             console.log('banco criado');
         }catch(err){
             console.log(err);
