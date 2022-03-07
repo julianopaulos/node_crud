c7bf51346235842e572626c4797ebe9173269c8c
julianopaulos <julianopaulo.santos@hotmail.com>
2022-03-07 07:06:34 -0300

    criando estrutura para testes unitários

--- tests/routes/store.spec.js
+++ tests/routes/store.spec.js
@@ -8,3 +8,11 @@ describe("register", async function() {
         equal(result, 5);*/
     });
 });
+
+describe("list", function() {
+    it("should return the code 401.", async function() {
+        const response = await fetch("http://localhost:3333/stores");
+        const responseBody = await response.json();
+        console.log(responseBody);
+    });
+});
