4fb6a3a2224119630f4118f74bd696d88f8187fe
julianopaulos <julianopaulo.santos@hotmail.com>
2022-03-08 06:30:29 -0300

    criados alguns testes para lojas e produtos

--- tests/routes/product.spec.js
+++ tests/routes/product.spec.js
@@ -1,9 +1,30 @@
 import fetch from 'cross-fetch';
 import { equal } from 'assert';
-
-describe("register", async function() {
-    it("should return the value 5 when the values 2 and 3 are passed.", function() {
-        /*const result = add(2, 3);
-        equal(result, 5);*/
+describe("product", function () {
+    describe("register", async function() {
+        it("should return the value 5 when the values 2 and 3 are passed.", function() {
+            /*const result = add(2, 3);
+            equal(result, 5);*/
+        });
     });
+
+    describe("list", function() {
+        it("should return the code 400.", async function() {
+            const response = await fetch("http://localhost:3333/products");
+            equal(response.status, 400);
+        });
+    
+        it("should return the code 401.", async function() {
+            const response = await fetch("http://localhost:3333/products", {
+                method: "GET",
+                headers: {
+                    authorization: "abv"
+                }
+            });
+            
+            //const responseBody = await response.json();
+            equal(response.status, 401);
+        });
+    });
+    
 });
--- tests/routes/store.spec.js
+++ tests/routes/store.spec.js
@@ -5,8 +5,20 @@ describe("store", function() {
     describe("list", function() {
         it("should return the code 400.", async function() {
             const response = await fetch("http://localhost:3333/stores");
-            const responseBody = await response.json();
-            equal(responseBody.statusCode, 400);
+            //const responseBody = await response.json();
+            //equal(responseBody.statusCode, 400);
+            equal(response.status, 400);
+        });
+
+        it("should return the code 401.", async function() {
+            const response = await fetch("http://localhost:3333/stores", {
+                method: "GET",
+                headers: {
+                    authorization: "abv"
+                }
+            });
+            //const responseBody = await response.json();
+            equal(response.status, 401);
         });
     });
 });
