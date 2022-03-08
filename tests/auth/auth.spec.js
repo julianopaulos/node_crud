2e4704852f25849f966c8ab7301bdee4bd9f7921
julianopaulos <julianopaulo.santos@hotmail.com>
2022-03-08 06:14:14 -0300

    começando criação dos testes

--- tests/auth/auth.spec.js
+++ tests/auth/auth.spec.js
@@ -1,10 +1,9 @@
 import fetch from 'cross-fetch';
 import { equal } from 'assert';
-import add from '../../src/operations/add.js';
 
-describe("register", async function() {
+/*describe("register", async function() {
     it("should return the value 5 when the values 2 and 3 are passed.", function() {
-        /*const result = add(2, 3);
-        equal(result, 5);*/
+        const result = add(2, 3);
+        equal(result, 5);
     });
-});
+});*/
--- tests/routes/store.spec.js
+++ tests/routes/store.spec.js
@@ -1,18 +1,12 @@
 import fetch from 'cross-fetch';
 import { equal } from 'assert';
-import add from '../../src/operations/add.js';
 
-describe("register", async function() {
-    it("should return the value 5 when the values 2 and 3 are passed.", function() {
-        /*const result = add(2, 3);
-        equal(result, 5);*/
-    });
-});
-
-describe("list", function() {
-    it("should return the code 401.", async function() {
-        const response = await fetch("http://localhost:3333/stores");
-        const responseBody = await response.json();
-        console.log(responseBody);
+describe("store", function() {
+    describe("list", function() {
+        it("should return the code 400.", async function() {
+            const response = await fetch("http://localhost:3333/stores");
+            const responseBody = await response.json();
+            equal(responseBody.statusCode, 400);
+        });
     });
 });
