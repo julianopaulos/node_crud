de2420deff48e338df79a5b2b228453d117a4d22
julianopaulos <julianopaulo.santos@hotmail.com>
2022-03-09 06:08:16 -0300

    adicionado mais um teste de produto

--- tests/routes/product.spec.js
+++ tests/routes/product.spec.js
@@ -1,12 +1,9 @@
 import fetch from 'cross-fetch';
 import { equal } from 'assert';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
 describe("product", function () {
-    describe("register", async function() {
-        it("should return the value 5 when the values 2 and 3 are passed.", function() {
-            /*const result = add(2, 3);
-            equal(result, 5);*/
-        });
-    });
 
     describe("list", function() {
         it("should return the code 400.", async function() {
@@ -22,9 +19,24 @@ describe("product", function () {
                 }
             });
             
-            //const responseBody = await response.json();
             equal(response.status, 401);
         });
+
+        it("should return the code 200", async function() {
+            const token = jwt.sign({ id: 1 }, process.env.SECRET_KEY, {
+                expiresIn: 6000 //sec
+            });
+
+            const response = await fetch("http://localhost:3333/products", {
+                method: "GET",
+                headers: {
+                    authorization: token
+                }
+            });
+            
+            equal(response.status, 200);
+        });
+
     });
     
 });
